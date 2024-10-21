aff573b39508928346702752f180704db35da25e
Cathrine Nesterova <nev.hrd@gmail.com>
2024-10-21 21:24:36 +0300

    Каждая картинка вызывается в новом окне

--- Cataas.py
+++ Cataas.py
@@ -23,14 +23,19 @@ def load_image(url):
         print(f"Ошибка при загрузке изображения: {e}")
         return None
 
-def set_image():
+def open_new_window():
     img = load_image(url)  # загрузка картинки с url
 
     if img:
-        # Устанавливаем изображение в метку
-        label.config(image=img)
-        # Необходимо сохранить ссылку на изображение, чтобы избежать сборки мусора
-        label.image = img
+        # Создаем новое вторичное окно
+        new_window = Toplevel()
+        new_window.title("Картинка с котиком")
+        new_window.geometry("600x520")
+
+        # Добавляем изображение в новое окно
+        label = Label(new_window, image=img)
+        label.pack()
+        label.image = img  # Сохраняем ссылку на изображение
 
 
 def exit():
@@ -41,11 +46,11 @@ window = Tk()
 window.title('Cats')
 window.geometry('600x520')
 
-# Создаем метку без изображения
+'''# Создаем метку без изображения
 label = Label()
-label.pack()
+label.pack()'''
 
-# update_button = Button(text='Обновить', command=set_image)
+# update_button = Button(text='Обновить', command=open_new_window)
 # update_button.pack()
 
 # Создаем меню
@@ -55,14 +60,10 @@ window.config(menu=menu_bar)
 # Добавляем пункты меню
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
-
-
 url = 'https://cataas.com/cat'
-
-set_image()
 
 window.mainloop()
